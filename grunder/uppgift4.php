82fc27ddd8e1f644e96d0f7803bd8a8344a32edb
Domaiki <sergeantsweden@gmail.com>
2021-09-28 13:33:59 +0200

    php gästbok och backend

---
+++ grunder/uppgift4.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    // echo "Klockan är just nu" . " ";
+    // echo date("H:i:s");
+    $tid = date ("H:i:s");
+    echo "Klockan är just nu $tid";
+    ?>
+</body>
+</html>
---
+++ net/backend.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gästboken</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="kontainer">
+        <h1 class="display-4">Gästboken</h1>
+        <?php
+        //Ta emot data som skickas
+        $ip = filter_input(INPUT_POST, 'ip', FILTER_SANITIZE_STRING);
+        $subnetmask = filter_input(INPUT_POST, 'subnetmask', FILTER_SANITIZE_STRING);
+        $gateway = filter_input(INPUT_POST, 'gateway', FILTER_SANITIZE_STRING);
+
+        //Finns data?
+        if ($ip && $subnetmask && $gateway) {
+            //Filnamnet
+            $filnamn = 'net.txt';
+
+            //Texten att spara
+            $texten = "ip: $ip\n";
+            $texten .= "subnetmask: $subnetmask\n";
+            $texten .= "gateway: $gateway";
+
+            //Spara i textfil
+            file_put_contents($filnamn, $texten);
+
+            //Bekräftelse
+            echo "<p class=\"alert alert-success\">Meddelandet har sparats!</p>";
+        } else {
+            echo "<p class=\"alert alert-warning\">Inget har sparats!</p>";
+        }
+        ?>
+    </div>
+</body>
+</html>
